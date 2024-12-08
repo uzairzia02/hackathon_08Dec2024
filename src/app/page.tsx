554b61c334c07366bd48f2070304d8667a250884
Uzair Zia <159820097+uzairzia02@users.noreply.github.com>
2024-12-09 01:17:25 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import Header from "./components/header";
+import Banner1 from "./homePage/banner1/page";
+import Ranges from "./homePage/ranges/ranges";
+import OurProducts from "./homePage/ourproducts/page";
+import Inspiration from "./homePage/inspiration/page";
+import Banner5 from "./homePage/banner5/page";
+import Footer from "./components/footer";
+
+export default function Home() {
+  return (
+  <>
+    
+  <Header />
+  <Banner1 />
+  <Ranges />
+  <OurProducts />
+  <Inspiration /> 
+  <Banner5 />
+  <Footer />
+
+  </>
+  );
+}
